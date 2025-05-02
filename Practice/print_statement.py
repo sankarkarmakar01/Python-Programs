20a7999c78a7d8b47dacd694bb3cfc6df94e50f0
sankarkarmakar01 <karmakarsankar000@gmail.com>
2025-05-02 21:58:40 +0530

    first commit

---
+++ Practice/print_statement.py
@@ -0,0 +1,9 @@
+# Hey, Please don't remove this line
+"""
+I am a multiline comment
+I am basically not read by the compiler
+"""
+print("Hey I am a good boy \nI am a good programmer")
+print("Hello\"")
+print("Hey",6,7,sep="~", end="009\n")
+print("Sankar")
---
+++ Practice/type_casting.py
@@ -0,0 +1,8 @@
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a) + int(b))
+
+# implicit type casting
+print(1015 + 32.55 + 859.2634556 + 3556555)
